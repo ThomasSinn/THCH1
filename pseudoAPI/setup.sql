d20158d07eba5281a3f913d4dbb06b9e15c0294e
Thomas Sinn <z5213546@unsw.edu.au>
2020-04-22 23:57:57 +1000

    minor

--- pseudoAPI/setup.sql
+++ pseudoAPI/setup.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*Creates tables and inserts data into the database for testing purposes*/
 
 CREATE TABLE food(
@@ -7,17 +6,3 @@ CREATE TABLE food(
     cuisine varchar(40) NOT NULL
 )
 
-=======
-/*Creates tables and inserts data into the database for testing purposes*/
-
-CREATE TABLE food(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    r_name varchar(256) NOT NULL,
-    r_lat decimal(3, 7),
-    r_lng decimal(3, 7),
-    name varchar(256) NOT NULL,
-    service varchar(256) NOT NULL,
-    price int NOT NULL
-)
-
->>>>>>> master
